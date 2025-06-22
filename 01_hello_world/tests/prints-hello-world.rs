d0b241369b2e1d9f03f7d86b04040d8e8389558b
AdamBouhmad <adbouhmad@gmail.com>
2025-06-21 19:45:19 -0700

    example one complete

--- 01_hello_world/tests/prints-hello-world.rs
+++ 01_hello_world/tests/prints-hello-world.rs
@@ -1,5 +1,9 @@
+use assert_cmd::Command;
+
 #[test]
-fn prints_hello_world() -> Result<(), Box<dyn std::error::Error>> { 
-    todo!("put your integration test code here!");
+fn prints_hello_world() -> Result<(), Box<dyn std::error::Error>> {
+    Command::cargo_bin("hello_world")? //build and run this binary
+        .assert() //validate output
+        .stdout("hello world!\n"); //assert that the stdout matches this text exactly
     Ok(())
 }
